cd4b4887c3f0cab030632ea3bccd5f888b213026
SahabMAlharbi <Sahab.Mubarak.ruh.s@tuwaiq.edu.sa>
2021-12-02 17:06:30 +0300

    camera feature modified

--- CameraApp/CameraApp/PhotoVC.swift
+++ CameraApp/CameraApp/PhotoVC.swift
@@ -12,7 +12,10 @@ class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigation
     
     let name = UILabel(frame: CGRect(x: 40, y: 200, width: 300, height: 30))
     var passedName = ""
-    let image = UIImageView()
+//    let imageName = "photo"
+//    let image = UIImage(named: imageName)
+    let imageView = UIImageView()
+
     let takeAPicture = UIButton(frame: CGRect(x: 20, y: 500, width: 370, height: 80))
     let cameraView = UIImagePickerController()
     init(name: String){
@@ -31,7 +34,7 @@ class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigation
         name.textColor = .white
         // Camera view
         
-//        self.present(cameraView, animated: true, completion: nil)
+        self.present(cameraView, animated: true, completion: nil)
         
         //Button
         takeAPicture.backgroundColor = .red
@@ -42,17 +45,17 @@ class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigation
         // AddSubview
         view.addSubview(name)
         view.addSubview(takeAPicture)
-        view.addSubview(image)
+        view.addSubview(imageView)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-        image.image = pickedImage
+            imageView.image = pickedImage
         picker.dismiss(animated: true, completion: nil)
         } else {
             print("Error")
         }
-        
+
         
     }
     @objc func openCamera(){
