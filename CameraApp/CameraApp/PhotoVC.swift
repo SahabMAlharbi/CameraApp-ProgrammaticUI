b101ed4f190d107ed5a5ed7b09a38c3419d2e9dc
SahabMAlharbi <Sahab.Mubarak.ruh.s@tuwaiq.edu.sa>
2021-12-02 16:07:00 +0300

    camera feature added

--- CameraApp/CameraApp/PhotoVC.swift
+++ CameraApp/CameraApp/PhotoVC.swift
@@ -8,11 +8,13 @@
 import Foundation
 import UIKit
 
-class PhotoVC : UIViewController{
+class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
     
     let name = UILabel(frame: CGRect(x: 40, y: 200, width: 300, height: 30))
     var passedName = ""
-    
+    let image = UIImageView()
+    let takeAPicture = UIButton(frame: CGRect(x: 20, y: 500, width: 370, height: 80))
+    let cameraView = UIImagePickerController()
     init(name: String){
         self.passedName = name
         super.init(nibName: nil, bundle: nil)
@@ -27,10 +29,37 @@ class PhotoVC : UIViewController{
         view.backgroundColor = .systemBlue
         name.text = passedName
         name.textColor = .white
+        // Camera view
         
+//        self.present(cameraView, animated: true, completion: nil)
+        
+        //Button
+        takeAPicture.backgroundColor = .red
+        takeAPicture.setTitle("Take A Picture", for: .normal)
+        takeAPicture.setTitleColor(UIColor.white, for: .normal)
+        takeAPicture.layer.cornerRadius = 10
+        takeAPicture.addTarget(self, action: #selector(openCamera), for: .touchUpInside)
+        // AddSubview
         view.addSubview(name)
+        view.addSubview(takeAPicture)
+        view.addSubview(image)
     }
     
-    
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        image.image = pickedImage
+        picker.dismiss(animated: true, completion: nil)
+        } else {
+            print("Error")
+        }
+        
+        
+    }
+    @objc func openCamera(){
+        cameraView.delegate = self
+        cameraView.sourceType = .camera
+        cameraView.allowsEditing = false
+        cameraView.showsCameraControls = true
+        self.present(cameraView, animated: true, completion: nil)
+    }
 }
