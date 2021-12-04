99b317b0760e7b66c01f41a50f7a76ade6dce5b8
WjdanMo <dannao@DANs-MacBook-Air.local>
2021-12-04 22:22:41 +0300

    Animated LaunchScreen, Saving photo to Camera Roll, and Implemented Code Refactoring

--- CameraApp/CameraApp/LaunchScreen.swift
+++ CameraApp/CameraApp/LaunchScreen.swift
@@ -7,16 +7,27 @@
 
 import Foundation
 import UIKit
+import Lottie
 
 class LaunchScreen : UIViewController{
     
-    let logo = UIImageView(image: UIImage(named: "appIcon"))
+    var animationview : AnimationView?
     
     override func viewDidLoad() {
-        view.backgroundColor = .white
-        logo.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        logo.center = view.center
-        view.addSubview(logo)
+
+        view.backgroundColor = #colorLiteral(red: 0.2056434453, green: 0.5644673109, blue: 0.8626580834, alpha: 1)
+        
+        animationview = .init(name: "camShot")
+        animationview?.frame = view.bounds
+        animationview?.loopMode = .loop
+        animationview?.animationSpeed = 1
+
+        animationview?.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
+        animationview?.center = self.view.center
+
+        view.addSubview(animationview!)
+        animationview?.play()
+        
     }
     
     
--- CameraApp/CameraApp/PhotoVC.swift
+++ CameraApp/CameraApp/PhotoVC.swift
@@ -10,12 +10,12 @@ import UIKit
 
 class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
     
-    let name = UILabel(frame: CGRect(x: 38, y: 80, width: 300, height: 30))
-    var passedName = ""
-    
-    let imageView = UIImageView(frame: CGRect(x: 10, y: 140, width: 360, height: 500))
-    let takeAPicture = UIButton(frame: CGRect(x: 90, y: 660, width: 200, height: 40))
+    let name = UILabel()
+    let imageView = UIImageView()
+    let takeAPicture = UIButton()
     let cameraView = UIImagePickerController()
+    let savePicture = UIButton()
+    var passedName = ""
     
     init(name: String){
         self.passedName = "\(name)'s Photo"
@@ -24,35 +24,22 @@ class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigation
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-        // super.init(coder: coder)
     }
     
     override func viewDidLoad() {
+        
         view.backgroundColor = .white
-        name.text = passedName
-        name.textColor = .black
-        name.textAlignment = .center
-        name.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17)
-        
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 8
-
-        takeAPicture.backgroundColor = .black
-        takeAPicture.setTitle("Take A Picture", for: .normal)
-        takeAPicture.setTitleColor(UIColor.white, for: .normal)
-        takeAPicture.layer.cornerRadius = 20
-        takeAPicture.addTarget(self, action: #selector(openCamera), for: .touchUpInside)
-        
-        view.addSubview(name)
-        view.addSubview(takeAPicture)
-        view.addSubview(imageView)
+        setupObjects()
         
     }
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             imageView.image = pickedImage
+            savePicture.isHidden = false
             picker.dismiss(animated: true, completion: nil)
+            
         } else {
             print("Error")
         }
@@ -64,5 +51,61 @@ class PhotoVC : UIViewController, UIImagePickerControllerDelegate & UINavigation
         cameraView.allowsEditing = false
         cameraView.showsCameraControls = true
         self.present(cameraView, animated: true, completion: nil)
+    }
+    
+    @objc func save(_ sender: AnyObject) {
+        guard let selectedImage = imageView.image else {
+            print("Image not found!")
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(selectedImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ imagee: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer){
+        if let error = error{
+            let ac = UIAlertController(title: "Error while saving", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Success!", message: "The Image has been saved successfully", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    
+    func setupObjects(){
+        
+        name.text = passedName
+        name.textColor = .black
+        name.frame = CGRect(x: 38, y: 80, width: 300, height: 30)
+        name.textAlignment = .center
+        name.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17)
+        view.addSubview(name)
+        
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 18, y: 140, width: 340, height: 480)
+        imageView.layer.cornerRadius = 8
+        imageView.image = UIImage(named: "No photos yet")
+//        imageView.contentMode = .scaleAspectFit
+        view.addSubview(imageView)
+        
+        takeAPicture.setTitle("Take A Picture", for: .normal)
+        takeAPicture.setTitleColor(UIColor.white, for: .normal)
+        takeAPicture.backgroundColor = #colorLiteral(red: 0.2056434453, green: 0.5644673109, blue: 0.8626580834, alpha: 1)
+        takeAPicture.frame = CGRect(x: 90, y: 640, width: 200, height: 40)
+        takeAPicture.layer.cornerRadius = 20
+        takeAPicture.addTarget(self, action: #selector(openCamera), for: .touchUpInside)
+        view.addSubview(takeAPicture)
+        
+        savePicture.setTitle("Save to your camera roll ", for: .normal)
+        savePicture.backgroundColor = #colorLiteral(red: 0.9119984508, green: 0.9065770507, blue: 0.9161657691, alpha: 0.8980392157)
+        savePicture.setTitleColor(.black, for: .normal)
+        savePicture.frame = CGRect(x: 60, y: 690, width: 260, height: 40)
+        savePicture.layer.cornerRadius = 20
+        savePicture.addTarget(self, action: #selector(save), for: .touchUpInside)
+        savePicture.isHidden = true
+        view.addSubview(savePicture)
+        
     }
 }
